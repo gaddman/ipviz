c31288870058299d3ee4454eb7117303b688733e
Chris Gadd <gaddman@email.com>
2014-04-21 14:30:27 +1200

    Python 3 now, tested Pillow OK

--- ipviz.py
+++ ipviz.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python3
 
 # Given a list of subnets, produce a visualisation of them.
 # IPv4 only
@@ -13,11 +13,15 @@ from random import random
 from PIL import Image, ImageDraw
 from colorsys import hsv_to_rgb
 
-grc = 0.618033988749895 # golden ratio conjugate
+# constants for the image construction
+grc = 0.618033988749895 # golden ratio conjugate (this may be a little more precise than necessary)
 rh = 10					# row height
+lineCol = "#C0C0C0"		# colour of dividing line
+legendCol = "#000000"	# colour of legend text overlay
 
+# define and parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument ("inputFile", help="a file list of IP ranges")
+parser.add_argument ("inputFile", help="a file list of IP ranges and optional labels")
 parser.add_argument ("-s", "--subnetSize", help="smallest size of subnet to print", default=24, type=int)
 parser.add_argument ("-S", "--supernetSize", help="subnet size at which to wrap to next line", type=int)
 parser.add_argument ("-b", "--blobSize", help="size of blob representing the smallest subnet", default=4, type=int)
@@ -29,6 +33,7 @@ parser.add_argument ("-o", "--outputFile", help="filename for output image", def
 args = parser.parse_args()
 globals().update(vars(args))
 
+# read file of networks and optional labels
 ipranges = [line.split() for line in open(inputFile)]
 
 # set default label where missing and create colour dictionary
@@ -39,17 +44,15 @@ for thisRange in ipranges:
 	except IndexError:
 		thisRange.append("O")
 	if thisRange[1] not in labels:
-		hue = random() # use random start value
-		hue = (hue+grc) % 1 # based on http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+		# from http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+		hue = random()
+		hue = (hue+grc) % 1
 		labels[thisRange[1]] = hue
-
-#print (ipranges)
-#print (labels)
 
 # convert string to IPv4Network
 networks = [[ipaddress.ip_network(thisRange[0]),thisRange[1]] for thisRange in ipranges]
 networks.sort()
-#print (networks)
+
 # set supernetSize to match smallest prefix
 if (not supernetSize):
 	supernetSize = min([thisNet[0].prefixlen for thisNet in networks])
@@ -63,17 +66,14 @@ for thisNet in networks:
 	else:
 		# our network of interest is smaller or equal to the supernet we want to display, drop it straight in
 		superNets.append(thisNet[0].supernet(new_prefix=supernetSize))
-# now remove dupes and sort
-superNets = sorted(list(set(superNets)))
-#print ([thisNet[0].exploded for thisNet in superNets])
+superNets = sorted(list(set(superNets)))	# remove dupes and sort
 
-# now we know how wide and tall the image will be
-bt = (0,-rh)					# top left of blob
-bb = (bt[0]+blobSize,bt[1]+rh)	# bottom right of blob
-lineCol = "#C0C0C0"		# colour of dividing line
+# now we know how wide and tall to create the image
 size = (blobSize*len(list(superNets[0].subnets(new_prefix = subnetSize))), rh*len(list(superNets))+rh*len(labels))
 img = Image.new('RGB',size)
 draw = ImageDraw.Draw(img)
+bt = (0,-rh)		# top left of blob, initial value for printing legend
+bb = (blobSize,0)	# bottom right of blob, initial value for printing legend
 
 # print legend
 if (textOut):
@@ -81,20 +81,20 @@ if (textOut):
 	print ([thisNet[0].exploded for thisNet in networks])
 	print ()
 	print ("Each row is a /{} and each point represents a /{} subnet".format(supernetSize,subnetSize))
-	print (". = no network, lowercase = partially covered, uppercase = completely covered\n")
+	print (". = no network, lowercase = partial subnet, uppercase = full subnet\n")
 for thisLabel in labels:
 	# full subnet
 	bt=(0,bt[1]+rh)
 	bb=(img.size[0]/2,bt[1]+rh)
 	netFill = tuple(int(x*256) for x in hsv_to_rgb(labels[thisLabel], 0.99, 0.99))
 	draw.rectangle([bt,bb], fill=netFill)
-	draw.text(bt,text=thisLabel+" full subnet",fill="#000000")
+	draw.text(bt,text=thisLabel+" full subnet",fill=legendCol)
 	# partial subnet
 	bt=(img.size[0]/2,bt[1])
 	bb=(img.size[0],bb[1])
 	netFill = tuple(int(x*256) for x in hsv_to_rgb(labels[thisLabel], 0.3, 0.99))
 	draw.rectangle([bt,bb], fill=netFill)
-	draw.text(bt,text=thisLabel+" partial subnet",fill="#000000")
+	draw.text(bt,text=thisLabel+" partial subnet",fill=legendCol)
 bt=(0,bt[1]+rh)
 bb=(blobSize,bt[1]+rh)
 
@@ -103,9 +103,8 @@ for thisSuper in superNets:
 	if (textOut):
 		print (thisSuper)
 
-	# find matching networks of interest
+	# find matching networks of interest in this supernet (for speed)
 	inclNets = [thisNet for thisNet in networks if thisSuper.overlaps(thisNet[0])]
-#	print (inclNets)
 
 	# determine all the subnets in this supernet
 	subNets = thisSuper.subnets(new_prefix = subnetSize)
@@ -129,17 +128,19 @@ for thisSuper in superNets:
 				# step out of for loop once we've gone past the subnet under test
 				break
 			# no matching network
+
 		# print (text) or draw (image) this subnet
 		if (textOut):
-			print (netChar,end="") # python3
+			print (netChar,end="")
 		#print (bt,netFill)
 		draw.rectangle([bt,bb], fill=netFill)
 		draw.line([bt,(bt[0],bt[1]+rh)],fill=lineCol,width=1)
 		bt=(bt[0]+blobSize,bt[1])
 		bb=(bt[0]+blobSize,bt[1]+rh)
+
 	# end of supernet, print EOL (text) or print network (image)
 	if (textOut):
-		print()	# python3
+		print()
 	draw.text((0,bt[1]),text=thisSuper.exploded,fill=lineCol)
 	bt=(0,bt[1]+rh)
 	bb=(blobSize,bt[1]+rh)
